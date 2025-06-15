0013543431204800da18a629fa0f9770a680bf16
Denis Queiroz <denisqueiroz1@gmail.com>
2025-06-14 20:02:34 -0400

    Criando  classe cidade e estados

--- backend/src/main/java/com/decati/sistema/SistemaApplication.java
+++ backend/src/main/java/com/decati/sistema/SistemaApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.decati.sistema.domain.Categoria;
+import com.decati.sistema.domain.Cidade;
+import com.decati.sistema.domain.Estado;
 import com.decati.sistema.domain.Produto;
 import com.decati.sistema.repositories.CategoriaRepository;
+import com.decati.sistema.repositories.CidadeRepository;
+import com.decati.sistema.repositories.EstadoRepository;
 import com.decati.sistema.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class SistemaApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository ;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;	
+	@Autowired
+    private EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SistemaApplication.class, args);
@@ -43,6 +51,19 @@ public class SistemaApplication implements CommandLineRunner {
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
